f04b12f26b836b0ec756a7811d35d0e5f2878567
felix-hcl <felix.muller@hcl.com>
2024-04-16 16:40:26 +0200

    adjusted license header

--- builder.js
+++ builder.js
@@ -8,10 +8,20 @@ const createIconContent = (iconName, size, subFolder, isRenamed, orignalName) =>
   if (!_.isEmpty(subFolder)) {
     utilsImport = `'../../../../utils'`;
   }
-  return `/* ========================================================================== *
-* Copyright (C) 2024 HCL America Inc.                                        *
-* Apache-2.0 license   https://www.apache.org/licenses/LICENSE-2.0           *
-* ========================================================================== */
+  return `/* ======================================================================== *
+  * Copyright 2024 HCL America Inc.                                          *
+  * Licensed under the Apache License, Version 2.0 (the "License");          *
+  * you may not use this file except in compliance with the License.         *
+  * You may obtain a copy of the License at                                  *
+  *                                                                          *
+  * http://www.apache.org/licenses/LICENSE-2.0                               *
+  *                                                                          *
+  * Unless required by applicable law or agreed to in writing, software      *
+  * distributed under the License is distributed on an "AS IS" BASIS,        *
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+  * See the License for the specific language governing permissions and      *
+  * limitations under the License.                                           *
+  * ======================================================================== */
 
 /* auto generated file - do not edit */
 import Icon from '@carbon/icons/es${!_.isEmpty(subFolder) ? `/${subFolder}` : ''}/${isRenamed ? orignalName: iconName}/${size}';
